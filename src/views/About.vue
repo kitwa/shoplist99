be286ffaa3bb970af4e40372e8d7939d04f144ae
kitwa <dominique@lucidocean.com>
2023-12-07 23:42:56 +0200

    Fix text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about pa-6">
     <h1>About {{ $store.state.appTitle }}</h1>
-    <p>LThis project was build just for learning purposes but can be use for doing shopping or for a to do list</p>
+    <p>This project was build just for learning purposes but can be use for doing shopping or for a to do list</p>
   </div>
 </template>
